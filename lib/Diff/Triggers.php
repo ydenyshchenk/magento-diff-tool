72a9159556cfac618e9ab596eb50dd5aa1d629ea
Yuriy Denyshchenko <ydenyshchenk@ebay.com>
2015-04-22 15:19:33 +0300

    Diff-Triggers: added 'CREATE TRIGGER' SQL generator for missed triggers (with auto prefix detector)

--- lib/Diff/Triggers.php
+++ lib/Diff/Triggers.php
@@ -3,6 +3,9 @@
 class Diff_Triggers extends Diff_Abstract
 {
     protected $_triggersList = false;
+    protected $_mviewEvents = false;
+    protected $_mviewEventIds = false;
+    protected $_prefix = false;
 
     public function __construct($skipHtmlInit = false)
     {
@@ -28,7 +31,9 @@ class Diff_Triggers extends Diff_Abstract
                 $triggers[$t->Trigger] = array(
                     'trigger' => $t->Trigger,
                     'event' => $t->Event,
-                    'statement' => $t->Statement
+                    'statement' => $t->Statement,
+                    'table' => $t->Table,
+                    'timing' => $t->Timing
                 );
             }
             $this->_triggersList[$db] = $triggers;
@@ -40,6 +45,53 @@ class Diff_Triggers extends Diff_Abstract
         }
 
         return $this->_triggersList[$db];
+    }
+
+    protected function _detectPrefix($db)
+    {
+        $this->_db->query('use `' . $db . '`');
+        $table = $this->_db->query("show tables like '%core_config_data';")->fetchColumn(0);
+        $this->_prefix = preg_replace('/core_config_data/', '', $table);
+
+        return $this->_prefix;
+    }
+
+    protected function _getMviewEvents($db, $getIds = false)
+    {
+        if (!isset($this->_mviewEvents[$db]) || !isset($this->_mviewEventIds[$db])) {
+            $this->_db->query('use `' . $db . '`');
+            $table = $this->_db->query("show tables like '%enterprise_mview_event';")->fetchColumn(0);
+            if ($table == false) {
+                return false;
+            }
+            $eventsRaw = $this->_db->query('SELECT * from ' . $table)->fetchAll(PDO::FETCH_OBJ);
+            $events = array();
+            $eventIds = array();
+            foreach ($eventsRaw as $e) {
+                $events[(int)$e->mview_event_id] = $e->name;
+                $eventIds[$e->name] = (int)$e->mview_event_id;
+            }
+            ksort($events);
+            $this->_mviewEvents[$db] = $events;
+            $this->_mviewEventIds[$db] = $eventIds;
+        }
+        return ($getIds) ? $this->_mviewEventIds[$db] : $this->_mviewEvents[$db];
+    }
+
+    protected function _getMviewEventName($eventId, $db)
+    {
+        $events = $this->_getMviewEvents($db);
+        return (!empty($events[$eventId])
+            ? $events[$eventId] : false
+        );
+    }
+
+    protected function _getMviewEventId($event, $db)
+    {
+        $eventIds = $this->_getMviewEvents($db, true);
+        return (!empty($eventIds[$event])
+            ? $eventIds[$event] : false
+        );
     }
 
     public function renderForm($ee = '', $supee = '')
@@ -63,7 +115,7 @@ class Diff_Triggers extends Diff_Abstract
         return $html;
     }
 
-    public function run($eeVersion = '', $db = '', $prefix = '')
+    public function run($eeDb = '', $supeeDb = '', $prefix = '')
     {
         if (
             $_POST && isset($_POST['diff']) && !empty($_POST['diff'])
@@ -71,43 +123,103 @@ class Diff_Triggers extends Diff_Abstract
             && isset($_POST['diff']['supee']) && !empty($_POST['diff']['supee'])
         ) {
             $d = $_POST['diff'];
-            $eeVersion = $d['ee'];
-            $db = $d['supee'];
+            $eeDb = $d['ee'];
+            $supeeDb = $d['supee'];
+            $prefix = $this->_detectPrefix($supeeDb);
         }
 
-        if (empty($d) || empty($eeVersion) || empty($db)) {
+        if (empty($d) || empty($eeDb) || empty($supeeDb)) {
             echo $this->_renderForm();
             exit();
-        } elseif (!empty($eeVersion) && !empty($db)) {
-            echo $this->_renderForm($eeVersion, $db, 'result');
+        } elseif (!empty($eeDb) && !empty($supeeDb)) {
+            echo $this->_renderForm($eeDb, $supeeDb, 'result');
         }
 
-        $coreTriggers = $this->_getTriggersList($eeVersion);
-        $triggers = $this->_getTriggersList($db);
+        $coreTriggers = $this->_getTriggersList($eeDb);
+        $localTriggers = $this->_getTriggersList($supeeDb);
 
+        $triggersMerged = array();
+        foreach ($coreTriggers as $triggerName => $triggerData) {
+            $triggerName = preg_replace("/trg_/", "trg_$prefix", $triggerName);
+            $statement = $triggerData['statement'];
+            $statement = preg_replace_callback('/(INTO|JOIN|UPDATE) \`([a-z\_]+)\`/', function($matches) {
+                return $matches[1] . ' `' . $this->_prefix . $matches[2] . '`';
+            }, $statement);
+            $triggerData['statement'] = $statement;
+            $triggersMerged[$triggerName]['core'] = $triggerData;
+        }
+        foreach ($localTriggers as $triggerName => $triggerData) {
+            $triggersMerged[$triggerName]['local'] = $triggerData;
+        }
 
-
-        $html = '<table class="table-bordered table-results">';
-        $html .= '<thead><tr><th>Trigger</th><th>Event</th><th>Statement ('
-            . count($triggers) . ')</th><th>Default statement (' . count($coreTriggers) . ')</th></tr></thead><tbody>';
-
-        foreach ($triggers as $code => $t) {
-            $s = $t['statement'];
-            $cS = ((isset($coreTriggers[$code]['statement']) && $coreTriggers[$code]['statement']) ? $coreTriggers[$code]['statement'] : '');
-
-            if ($s == $cS) {
-                continue;
+        $missedTriggers = array();
+        $corruptedTriggers = array();
+        $triggersDiff = array();
+        foreach ($triggersMerged as $triggerName => $t) {
+            if (empty($t['local'])) {
+                $missedTriggers[$triggerName] = $t['core'];
+                $triggersDiff[$triggerName] = $t;
+            } elseif ($t['local']['statement'] != $t['core']['statement']) {
+                $corruptedTriggers[$triggerName] = $t;
+                $triggersDiff[$triggerName] = $t;
             }
-
-            $html .= '<tr>';
-            $html .= '<td>' . $t['trigger'] . '</td>'
-                . '<td>' . $t['event'] . '</td>'
-                . '<td style="color: ' . (($s != $cS) ? 'red' : '') . '">' . preg_replace('/\n/u', '<br>', $t['statement']) . '</td>'
-                . '<td>' . preg_replace('/\n/u', '<br>', $cS) . '</td>';
-
-            $html .= '</tr>';
         }
-        $html .= '</tbody></table>';
+
+
+        $html = '';
+        if ($triggersDiff) {
+            $html .= '<table class="table-bordered table-results">';
+            $html .= '<thead><tr><th>Trigger</th><th>Event</th><th>Local statement (count: '
+                . count($localTriggers) . ')</th><th>Default statement (count: ' . count($coreTriggers) . ')</th></tr></thead><tbody>';
+
+            foreach ($triggersDiff as $code => $t) {
+                $s = (!empty($t['local']['statement'])) ? $t['local']['statement'] : '';
+                $cS = (!empty($t['core']['statement'])) ? $t['core']['statement'] : '';
+                $event = (!empty($t['core']['event'])) ? $t['core']['event'] : '';
+
+                if ($s == $cS) {
+                    continue;
+                }
+
+                $html .= '<tr>';
+                $html .= '<td>' . $code . '</td>'
+                    . '<td>' . $event . '</td>'
+                    . '<td style="color: ' . (($s != $cS) ? 'red' : '') . '">' . (($s) ? preg_replace('/\n/u', '<br>', $s) : 'MISSED') . '</td>'
+                    . '<td>' . preg_replace('/\n/u', '<br>', $cS) . '</td>';
+
+                $html .= '</tr>';
+            }
+            $html .= '</tbody></table>';
+
+
+            if ($missedTriggers) {
+                $html .= '<div class="well well-lg">';
+                $html .= 'Triggers install script:';
+                $html .= '<textarea style="width: 100%;" rows="100">delimiter //' . "\n";
+
+                foreach ($missedTriggers as $triggerName => $t) {
+                    $tTime = $t['timing'];
+                    $tEvent = $t['event'];
+                    $tTable = $t['table'];
+                    $tStatement = $t['statement'];
+
+                    $tStatement = preg_replace_callback('/mview\_event\_id\s\=\s\'(\d+)\'/', function ($matches) {
+                        global $eeDb, $supeeDb;
+                        $coreEventId = (int)$matches[1];
+                        $mviewEvent = $this->_getMviewEventName($coreEventId, $eeDb);
+                        $eventId = $this->_getMviewEventId($mviewEvent, $supeeDb);
+
+                        return "mview_event_id = '$eventId'";
+                    }, $tStatement);
+
+                    $html .= "CREATE TRIGGER $triggerName\n$tTime $tEvent\nON $tTable FOR EACH ROW \n$tStatement//\n";
+                }
+                $html .= '</textarea>';
+                $html .= '</div>';
+            }
+        } else {
+            $html .= '<div class="pT60"><div class="well well-lg"><h1 class="m0">Triggers are identical</h1></div></div>';
+        }
 
         echo '<div class="container">' . $html . '</div>';
     }
